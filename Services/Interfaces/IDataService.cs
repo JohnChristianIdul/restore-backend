222f5cebc9106ef54513d4a53cb7b07f89cc3b33
JohnChristianIdul <idulchristian@gmail.com>
2024-10-17 19:36:45 +0800

    Add Email Verification and Reset Password

--- Services/Interfaces/IDataService.cs
+++ Services/Interfaces/IDataService.cs
@@ -18,7 +18,6 @@ namespace ReStore___backend.Services.Interfaces
         Task<string> GetSalesPrediction(string username);
         Task<string> GetDemandPrediction(string username);
         Task<string> SignUp(string email, string name, string username, string phoneNumber, string password);
-        Task SendVerificationEmailAsync(string email, string verificationLink);
         Task<bool> IsEmailVerified(string userId);
         Task<LoginResultDTO> Login(string email, string password);
         Task SendPasswordResetEmailAsync(string email);
